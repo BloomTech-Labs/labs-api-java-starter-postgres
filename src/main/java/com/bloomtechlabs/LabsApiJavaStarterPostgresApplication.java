cc23e22e829a96ae5f58f9b08fc92c3407987db5
Tom Tarpey <tomtarpeydev@gmail.comn>
2022-08-25 18:47:03 +0100

    initial commit

---
+++ src/main/java/com/bloomtechlabs/LabsApiJavaStarterPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.bloomtechlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsApiJavaStarterPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsApiJavaStarterPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloomtechlabs/LabsApiJavaStarterPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomtechlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsApiJavaStarterPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
